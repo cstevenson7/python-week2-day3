b89074413899079969f02dcfed56ab9759b76ac2
cstevenson7 <cindy.stevenson1@gmail.com>
2020-07-23 07:54:34 -0600

    add spelling ck to delete

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,8 +1,11 @@
+
 def shoppingCart():
-    #clear_output()
+    """Creates a list of purchases from user input.
+        the user can show/add/delete/quit"""   
     items = [] 
-         
+    #clear_output()     
     while True:
+       
         print("\nEnter what you want to do. ")
         print("The choices are: ")
         action = input("Show / Add / Delete or Quit: ")
@@ -23,8 +26,15 @@ def shoppingCart():
                 continue
             else:
                 items_delete = input("\nEnter item name an that you want to delete from your cart (this deletes all occurences) ")         
-                while items_delete in items:
-                    items.remove(items_delete)
+                if items_delete not in items:
+                    print("\nItems could not be found in your cart, please try again")
+                    items_delete = input("\nEnter item name an that you want to delete from your cart (this deletes all occurences) ")
+                    while items_delete in items:
+                        items.remove(items_delete)
+                else:
+                    while items_delete in items:
+                        items.remove(items_delete)
+                                     
         else:
             print('Your choice of what you wanted to do is not valid, please try again')
             break
